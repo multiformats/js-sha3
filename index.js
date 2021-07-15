c4e76634f2e8836abb222be5571ed4994b6d5a1e
Rod Vagg <rod@vagg.org>
2021-07-15 20:31:44 +1000

    feat: add types, update deps, semantic-release, dependabot

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 'use strict'
-import sha3 from 'js-sha3'
-const encoder = fn => b => new Uint8Array(fn.array(b))
 
-export default [
-  { code: 0x14, name: 'sha3-512', encode: encoder(sha3.sha3_512) },
-  { code: 0x15, name: 'sha3-384', encode: encoder(sha3.sha3_384) },
-  { code: 0x16, name: 'sha3-256', encode: encoder(sha3.sha3_256) },
-  { code: 0x17, name: 'sha3-224', encode: encoder(sha3.sha3_224) },
-  { code: 0x18, name: 'shake-128', encode: encoder(sha3.shake128) },
-  { code: 0x19, name: 'shake-256', encode: encoder(sha3.shake256) },
-  { code: 0x1a, name: 'keccak-224', encode: encoder(sha3.keccak224) },
-  { code: 0x1b, name: 'keccak-256', encode: encoder(sha3.keccak256) },
-  { code: 0x1c, name: 'keccak-384', encode: encoder(sha3.keccak384) },
-  { code: 0x1d, name: 'keccak-512', encode: encoder(sha3.keccak512) }
-]
+import { from } from 'multiformats/hashes/hasher'
+import sha3 from 'js-sha3'
+
+/**
+ * @param {sha3.Hash} fn
+ * @returns {(inp:Uint8Array)=>Uint8Array}
+ */
+function encoder (fn) {
+  return (/** @type {Uint8Array} */ b) => new Uint8Array(fn.array(b))
+}
+
+export const sha3512 = from({ code: 0x14, name: 'sha3-512', encode: encoder(sha3.sha3_512) })
+export const sha3384 = from({ code: 0x15, name: 'sha3-384', encode: encoder(sha3.sha3_384) })
+export const sha3256 = from({ code: 0x16, name: 'sha3-256', encode: encoder(sha3.sha3_256) })
+export const sha3224 = from({ code: 0x17, name: 'sha3-224', encode: encoder(sha3.sha3_224) })
+export const shake128 = from({ code: 0x18, name: 'shake-128', encode: (b) => new Uint8Array(sha3.shake128.array(b, 256)) })
+export const shake256 = from({ code: 0x19, name: 'shake-256', encode: (b) => new Uint8Array(sha3.shake256.array(b, 512)) })
+export const keccak224 = from({ code: 0x1a, name: 'keccak-224', encode: encoder(sha3.keccak224) })
+export const keccak256 = from({ code: 0x1b, name: 'keccak-256', encode: encoder(sha3.keccak256) })
+export const keccak384 = from({ code: 0x1c, name: 'keccak-384', encode: encoder(sha3.keccak384) })
+export const keccak512 = from({ code: 0x1d, name: 'keccak-512', encode: encoder(sha3.keccak512) })
--- test/test-basics.js
+++ test/test-basics.js
@@ -1,24 +1,55 @@
-/* globals describe, it */
-import main from '@multiformats/sha3'
-import assert from 'assert'
+/* eslint-env mocha */
 
-import { create } from 'multiformats'
+import * as sha3 from '@multiformats/sha3'
+import chai from 'chai'
+import { bytes } from 'multiformats'
+import table from './table.csv.js'
 
-const test = it
-const same = assert.deepStrictEqual
+const { assert } = chai
 
-const { multihash, bytes } = create()
-multihash.add(main)
+const fixtures = [
+  ['sha3-512', 'beep boop', '1440fae2c9eb19906057f8bf507f0e73ee02bb669d58c3069e7718b89ca4d314cf4fd6f1679019cc46d185c7af34f6c05a307b070e74e9ed5b9c64f86aacc2b90d10'],
+  ['sha3-384', 'beep boop', '153075a9cff1bcfbe8a7025aa225dd558fb002769d4bf3b67d2aaf180459172208bea989804aefccf060b583e629e5f41e8d'],
+  ['sha3-256', 'beep boop', '1620828705da60284b39de02e3599d1f39e6c1df001f5dbf63c9ec2d2c91a95a427f'],
+  ['sha3-224', 'beep boop', '171c0da73a89549018df311c0a63250e008f7be357f93ba4e582aaea32b8'],
+  // these two fixtures inherited from https://github.com/multiformats/js-multihashing-async/blob/master/test/fixtures/encodes.js
+  // are not correct, they have the wrong output length, the next two are from go-multihash
+  // ['shake-128', 'beep boop', '18105fe422311f770743c2e0d86bcca09211'],
+  // ['shake-256', 'beep boop', '192059feb5565e4f924baef74708649fed376d63948a862322ed763ecf093b63b38b'],
+  ['shake-128', 'foo', '1820f84e95cb5fbd2038863ab27d3cdeac295ad2d4ab96ad1f4b070c0bf36078ef08'],
+  ['shake-256', 'foo', '19401af97f7818a28edfdfce5ec66dbdc7e871813816d7d585fe1f12475ded5b6502b7723b74e2ee36f2651a10a8eaca72aa9148c3c761aaceac8f6d6cc64381ed39'],
+  ['keccak-224', 'beep boop', '1a1c2bd72cde2f75e523512999eb7639f17b699efe29bec342f5a0270896'],
+  ['keccak-256', 'beep boop', '1b20ee6f6b4ce5d754c01203cb3b99294b88615df5999e20d6fe509204fa254a0f97'],
+  ['keccak-384', 'beep boop', '1c300e2fcca40e861fc425a2503a65f4a4befab7be7f193e57654ca3713e85262b035e54d5ade93f9632b810ab88b04f7d84'],
+  ['keccak-512', 'beep boop', '1d40e161c54798f78eba3404ac5e7e12d27555b7b810e7fd0db3f25ffa0c785c438331b0fbb6156215f69edf403c642e5280f4521da9bd767296ec81f05100852e78']
+]
 
-const fixture = bytes.fromString('testFixture')
-
-for (const { name, code } of main) {
-  describe(name, () => {
-    test('encode', async () => {
-      const hashed = await multihash.hash(fixture, name)
-      await multihash.validate(hashed, fixture)
-      const duplicate = await multihash.hash(fixture, code)
-      same(hashed, duplicate)
+describe('Digests', () => {
+  for (const [name, input, expectedDigest] of fixtures) {
+    it(name, async () => {
+      const hasher = sha3[name.replace('-', '')]
+      const hash = await hasher.digest(bytes.fromString(input))
+      assert.strictEqual(hash.code, hasher.code)
+      assert.strictEqual(bytes.toHex(hash.bytes), expectedDigest)
     })
-  })
-}
+  }
+})
+
+describe('Hashers', () => {
+  const beepboop = bytes.fromString('beep boop')
+
+  const codecs = table.split('\n')
+    .filter(Boolean)
+    .map((l) => l.split(',').map((e) => e.trim()).map((e) => e.startsWith('0x') ? parseInt(e, 16) : e))
+
+  for (const [name,, code] of codecs) {
+    const exportName = name.replace('-', '')
+
+    it(name, async () => {
+      const hasher = sha3[exportName]
+      assert.strictEqual(hasher.code, code)
+      const hash = await hasher.digest(beepboop)
+      assert.strictEqual(hash.code, hasher.code)
+    })
+  }
+})
